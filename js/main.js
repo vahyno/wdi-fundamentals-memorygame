f8e818ef190b923450e29fdd07496f5e72615429
Michal <vahyno@gmail.com>
2018-01-24 17:32:08 -0800

    created game logic (Unit 8 Assignment)

--- js/main.js
+++ js/main.js
@@ -1,10 +1,32 @@
 console.log("Up and running!");
 
-var cardOne = "queen";
-var cardTwo = "queen";
-var cardThree = "king";
-var cardFour = "king";
+alert('Hello Friend, How about a nice game of Pexeso?');
 
-//console.log("test: "+ cardOne + ", " + cardTwo + ", " + cardThree + ", " + cardFour);
-console.log("User flipped " + cardOne);
-console.log("User flipped " + cardThree);
+var cards = ["queen", "queen", "king", "king"];
+var cardsInPlay = [];
+
+var cardOne = cards[0];
+var cardTwo = cards[1];
+var cardThree = cards[2];
+var cardFour = cards[3];
+
+cardsInPlay.push(cardOne);
+
+console.log(cards); //test
+
+console.log(cardsInPlay); //test
+console.log("User flipped " + cardsInPlay[0]);
+
+cardsInPlay.push(cardTwo); 
+
+console.log("User flipped " + cardsInPlay[1]);
+
+if (cardsInPlay.length === 2) {
+	console.log("User flipped 2 cards.") //test
+	if (cardOne === cardTwo) {
+		alert("You found match!");
+	}
+	else {
+		alert("Sorry, try again.");
+	}
+}
