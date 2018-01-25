2b16f333b8a18f628ecc26edceabf7f175928156
Michal <vahyno@gmail.com>
2018-01-24 20:21:50 -0800

    created game logic - functions Unit 9 Assignment

--- js/main.js
+++ js/main.js
@@ -5,7 +5,30 @@ alert('Hello Friend, How about a nice game of Pexeso?');
 var cards = ["queen", "queen", "king", "king"];
 var cardsInPlay = [];
 
-var cardOne = cards[0];
+function checkForMatch(){
+	if (cardsInPlay.length === 2){
+		if (cardsInPlay[0] === cardsInPlay[1]) {
+			console.log("You found a match!");
+			alert("You found a match!");
+		} else {
+			console.log("Sorry, try again.");
+			alert("Sorry, try again.");
+		}
+	}
+}
+
+function flipCard(cardId){
+	console.log("User flipped " + cards[cardId]+".");
+	cardsInPlay.push(cards[cardId]);
+	checkForMatch();
+}
+
+flipCard(0);
+flipCard(2);
+
+//checkForMatch();
+
+/*var cardOne = cards[0];
 var cardTwo = cards[1];
 var cardThree = cards[2];
 var cardFour = cards[3];
@@ -29,4 +52,4 @@ if (cardsInPlay.length === 2) {
 	else {
 		alert("Sorry, try again.");
 	}
-}
+}*/
